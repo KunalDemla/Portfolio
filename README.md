c226a31cdd8e82e279e029a6c964efcf0148b1d8
KunalDemla <kunaldemla@gmail.com>
2022-06-08 06:02:39 +0530

    all pages made, home page done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Portfolio
+
+Link to [My Portfolio](https://kunaldemla.github.io/Portfolio/).
+
+
+Made using HTML, CSS, JavaScript and Jquery along with Iframe from Spline.
---
+++ index.js
@@ -0,0 +1,66 @@
+$(".Invi").fadeOut(0);
+
+
+function TurnDisplay()
+{
+    $(".Invi").fadeIn(1000);
+}
+
+var a=0;
+
+
+$("#splineiframe").hover(function()
+{
+    $("#HoverIt").fadeOut();
+    a=1;
+})
+
+function goodbyesc()
+{
+    $("#scrolldown").fadeOut();
+}
+
+
+
+
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
